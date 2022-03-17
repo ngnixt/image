0a37c39207f35b2c8e2c4b809276fdb26e0a87ee
lukaonik <lukaonik@gmail.com>
2022-03-17 12:51:45 +0400

    initial commit

---
+++ apps/demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'image-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo';
+}
